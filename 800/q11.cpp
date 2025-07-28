35aa20968983d29849b19a704ffbce17cc7028a1
aggarwalharsh985 <aggarwalharsh985@gmail.com>
2025-07-28 22:50:49 +0530

    make a fastest solution

--- 800/q11.cpp
+++ 800/q11.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-#include<vector>
-#include<limits>
-#include<limits.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
     int n;
     cin>>n;
