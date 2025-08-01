5efb2190de086c5fd6a6825aa0f65cad85e4fc1e
aggarwalharsh985 <aggarwalharsh985@gmail.com>
2025-08-01 22:26:58 +0530

    add question 7

---
+++ 1100/q7.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+bool check(ll mid, vector<ll>&v, ll x){
+    ll units = 0;
+    ll n = v.size();
+    for(int i = 0; i<n; i++){
+        if(v[i]<mid){
+            units += (mid-v[i]);
+        }
+    }
+    return units<=x;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n,x;
+        cin>>n>>x;
+        vector<ll>v(n);
+        for(int i = 0; i<n; i++){
+            cin>>v[i];
+        }
+        ll s = 1, e = INT_MAX, ans = -1;
+        while(s<=e){
+            ll mid = s + (e-s)/2;
+            if(check(mid,v,x)){
+                ans = mid;
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+}
